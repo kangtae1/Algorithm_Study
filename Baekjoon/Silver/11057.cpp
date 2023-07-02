8a1e1582fcb0e32b67f8cd10d0fc38d17a0ceaa3
kangtae1 <kangtae1@icloud.com>
2023-07-02 17:25:18 +0900

    11057

---
+++ Baekjoon/Silver/11057.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int dp[1001][10] = {0, };
+    for (int i = 0; i < 10; i++) {
+        dp[1][i] = 1;
+    }
+
+    for (int i = 2; i <= N; i++) {
+        dp[i][0] = 1;
+        for (int j = 1; j < 10; j++) {
+            dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 10007;
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < 10; i++) {
+        sum += dp[N][i];
+    }
+
+    cout << sum % 10007 << endl;
+
+    return 0;
+}
